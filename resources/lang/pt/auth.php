84f3850105893f9bf6c96738a76cbe9adf0429ed
arthur571 <muscugamer@gmail.com>
2019-01-19 17:29:33 -0200

    Create auth.php
    Tradução-Português

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'not_authorized' => 'Você não está autorizado a executar esta ação.',
+    'auth_error' => 'Houve um erro ao tentar efetuar login.',
+    'authentication_required' => 'A autenticação é necessária para continuar.',
+    'remember_me' => 'Lembre de mim',
+    'sign_in' => 'Entrar',
+    'forgot_password' => 'I\'esqueci minha senha!',
+    'request_reset_text' => 'Esqueceu a senha da sua conta? Não é o fim do mundo, apenas forneça seu email abaixo.',
+    'reset_password_text' => 'Redefina a senha da sua conta.',
+    'reset_password' => 'Redefinir senha da conta',
+    'email_sent' => 'Um email foi enviado a você com mais instruções para redefinir sua senha.',
+    'failed' => 'As credenciais fornecidas não correspondem àquelas registradas no registro ou o token 2FA fornecido está inválido.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'password_requirements' => 'As senhas devem conter pelo menos um caractere maiúsculo, minúsculo e numérico e devem ter no mínimo 8 caracteres de comprimento.',
+    'request_reset' => 'Localizar conta',
+    '2fa_required' => '2-Autenticação de Fator',
+    '2fa_failed' => 'O token 2FA fornecido era inválido.',
+    'totp_failed' => 'Houve um erro ao tentar validar o TOTP.',
+    '2fa_must_be_enabled' => 'O administrador exigiu que a autenticação de dois fatores seja ativada para sua conta, a fim de usar o Painel.',
+];
